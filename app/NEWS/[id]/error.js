718618600d96a551c37d65fe3c5f6ceaa6118a66
iosebashviliggoga <iosebashviliggoga@gmail.com>
2023-04-24 10:34:35 +0400

    added error component

---
+++ app/NEWS/[id]/error.js
@@ -0,0 +1,22 @@
+'use server'
+
+export default async function Error({
+  error,
+  reset,
+}){
+  
+
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <button
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </button>
+    </div>
+  );
+}
